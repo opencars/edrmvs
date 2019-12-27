d29d6d545bf26124990e4b68cab5b230db3647f4
Ali Shanaakh <github@shal.dev>
2019-12-27 22:43:22 +0200

    Switch weight to float64 (REAL)

--- pkg/model/registration.go
+++ pkg/model/registration.go
@@ -11,24 +11,24 @@ import (
 
 // Registration represents database entity for vehicle registration.
 type Registration struct {
-	Brand        *string `db:"brand" json:"brand"`
-	Capacity     *int    `db:"capacity" json:"capacity"`
-	Color        string  `db:"color" json:"color"`
-	DFirstReg    *string `db:"d_first_reg" json:"d_first_reg"`
-	DReg         *string `db:"d_reg" json:"d_reg"`
-	Fuel         *string `db:"fuel" json:"fuel"`
-	Kind         *string `db:"kind" json:"kind"`
-	MakeYear     int     `db:"make_year" json:"make_year"`
-	Model        *string `db:"model" json:"model"`
-	NDoc         string  `db:"n_doc" json:"n_doc"`
-	SDoc         string  `db:"s_doc" json:"s_doc"`
-	NRegNew      string  `db:"n_reg_new" json:"n_reg_new"`
-	NSeating     *int    `db:"n_seating" json:"n_seating"`
-	NStanding    *int    `db:"n_standing" json:"n_standing"`
-	OwnWeight    *int    `db:"own_weight" json:"own_weight"`
-	RankCategory *string `db:"rank_category" json:"rank_category"`
-	TotalWeight  *int    `db:"total_weight" json:"total_weight"`
-	VIN          *string `db:"vin" json:"vin"`
+	Brand        *string  `db:"brand" json:"brand"`
+	Capacity     *int     `db:"capacity" json:"capacity"`
+	Color        string   `db:"color" json:"color"`
+	DFirstReg    *string  `db:"d_first_reg" json:"d_first_reg"`
+	DReg         *string  `db:"d_reg" json:"d_reg"`
+	Fuel         *string  `db:"fuel" json:"fuel"`
+	Kind         *string  `db:"kind" json:"kind"`
+	MakeYear     int      `db:"make_year" json:"make_year"`
+	Model        *string  `db:"model" json:"model"`
+	NDoc         string   `db:"n_doc" json:"n_doc"`
+	SDoc         string   `db:"s_doc" json:"s_doc"`
+	NRegNew      string   `db:"n_reg_new" json:"n_reg_new"`
+	NSeating     *int     `db:"n_seating" json:"n_seating"`
+	NStanding    *int     `db:"n_standing" json:"n_standing"`
+	OwnWeight    *float64 `db:"own_weight" json:"own_weight"`
+	RankCategory *string  `db:"rank_category" json:"rank_category"`
+	TotalWeight  *float64 `db:"total_weight" json:"total_weight"`
+	VIN          *string  `db:"vin" json:"vin"`
 }
 
 func FromHSC(registration hsc.Registration) (*Registration, error) {
@@ -52,12 +52,12 @@ func FromHSC(registration hsc.Registration) (*Registration, error) {
 		return nil, fmt.Errorf("nStanding: %w", err)
 	}
 
-	ownWeight, err := utils.Atoi(registration.OwnWeight)
+	ownWeight, err := utils.Atof(registration.OwnWeight)
 	if err != nil {
 		return nil, fmt.Errorf("ownWeight: %w", err)
 	}
 
-	totalWeight, err := utils.Atoi(registration.TotalWeight)
+	totalWeight, err := utils.Atof(registration.TotalWeight)
 	if err != nil {
 		return nil, fmt.Errorf("totalWeight: %w", err)
 	}
--- pkg/utils/integer.go
+++ pkg/utils/integer.go
@@ -16,3 +16,16 @@ func Atoi(lexeme *string) (*int, error) {
 
 	return &result, nil
 }
+
+func Atof(lexeme *string) (*float64, error) {
+	if lexeme == nil {
+		return nil, nil
+	}
+
+	result, err := strconv.ParseFloat(*lexeme, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return &result, nil
+}
