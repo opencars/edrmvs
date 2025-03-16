29db49a042bd8dca559126db959fa0d870e7afca
Ali Shanaakh <github@shal.dev>
2025-03-16 19:40:55 +0100

    Add reflection

--- pkg/api/grpc/api.go
+++ pkg/api/grpc/api.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/opencars/grpc/pkg/registration"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 
 	"github.com/opencars/edrmvs/pkg/domain"
 )
@@ -24,10 +25,14 @@ func New(addr string, svc domain.InternalService) *API {
 		),
 	}
 
+	server := grpc.NewServer(opts...)
+	// Enable reflection for debugging and development tools
+	reflection.Register(server)
+
 	return &API{
 		addr: addr,
 		svc:  svc,
-		s:    grpc.NewServer(opts...),
+		s:    server,
 	}
 }
 
